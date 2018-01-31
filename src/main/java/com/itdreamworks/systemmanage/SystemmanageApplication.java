1f5d926a3305f77d40063c126e87ca434f3085b5
Administrator <80201288@qq.com>
2018-01-31 20:11:24 +0800

    项目初始化

---
+++ src/main/java/com/itdreamworks/systemmanage/SystemmanageApplication.java
@@ -0,0 +1,12 @@
+package com.itdreamworks.systemmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemmanageApplication.class, args);
+	}
+}
